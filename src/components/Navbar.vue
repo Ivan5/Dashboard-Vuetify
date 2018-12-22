e7aaa1343835ef6bd531875f092bd5701af37f5a
Ivan5 <converse_ivan@hotmail.com>
2018-12-21 23:55:32 -0600

    Add a toolbar for the app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbal-title class="text-uppercase grey--text">
+        <span class="font-weight light">Todo</span>
+      </v-toolbal-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
