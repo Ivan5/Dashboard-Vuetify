5c6b7f7e5c7460fcb89f83a8a8ed5e48eaab5c4f
Ivan5 <converse_ivan@hotmail.com>
2018-12-23 19:11:12 -0600

    Validate Form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,9 +6,9 @@
         <h2>Add a new Project</h2>
       </v-card-title>
       <v-card-text>
-        <v-form class="px-3">
-          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+        <v-form class="px-3" ref="form">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
 
           <v-menu>
             <v-text-field
@@ -35,12 +35,15 @@ export default {
     return {
       title: "",
       content: "",
-      due: null
+      due: null,
+      inputRules: [v => v.length >= 3 || "Minimum length is 3 characters"]
     };
   },
   methods: {
     submit() {
-      console.log(this.title, this.content);
+      if (this.$refs.form.validate()) {
+        console.log(this.title, this.content);
+      }
     }
   },
   computed: {
